166e55070fb10d2b7fbb92d59b59ab222091f4b7
Rishikesh <rishikesh@eximuz.com>
2023-01-04 22:57:56 +0530

    Add new style folder and include styles inside homeComponent

--- src/components/HomeComponent.vue
+++ src/components/HomeComponent.vue
@@ -1,20 +1,22 @@
 <template>
-    <div>
-        <HeaderComponent />
-        <FooterComponent />
-      Home
-    </div>
-  </template>
+  <div>
+    <HeaderComponent />
+    <FooterComponent />
+    Home
+  </div>
+</template>
   
-  <script>
-  import HeaderComponent from './HeaderComponent.vue';
-  import FooterComponent from './FooterComponent.vue';
-  export default {
-    name: 'HomeComponent',
-    components: {
-        HeaderComponent,
-        FooterComponent
-    },
-  }
-  </script>
-  
+<script>
+import HeaderComponent from './HeaderComponent.vue';
+import FooterComponent from './FooterComponent.vue';
+export default {
+  name: 'HomeComponent',
+  components: {
+    HeaderComponent,
+    FooterComponent
+  },
+}
+</script>
+<style>
+@import "../assets/styles/common-style.css";
+</style>
