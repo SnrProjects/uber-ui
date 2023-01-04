6569fbe0b977bdd3fca9f90ff3cdd7d6e010331f
Snrprojects <Snrproject1@gmail.com>
2023-01-04 22:33:10 +0530

    removed unwanted text and added new components

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <HeaderComponent />
+        <FooterComponent />
+      Home
+    </div>
+  </template>
+  
+  <script>
+  import HeaderComponent from './HeaderComponent.vue';
+  import FooterComponent from './FooterComponent.vue';
+  export default {
+    name: 'HomeComponent',
+    components: {
+        HeaderComponent,
+        FooterComponent
+    },
+  }
+  </script>
+  
--- src/main.js
+++ src/main.js
@@ -2,10 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vuetify from 'vuetify'
 
 Vue.config.productionTip = false
 
+Vue.use(Vuetify)
+
+// new Vue({
+//   router,
+//   store,
+//   render: h => h(App)
+// }).$mount('#app')
+
 new Vue({
+  vuetify: new Vuetify({
+    icons: {
+      iconfont: "mdi"
+    }
+  }),
   router,
   store,
   render: h => h(App)
