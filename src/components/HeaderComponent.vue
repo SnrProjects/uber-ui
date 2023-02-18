03e64b36193834125af75eebe11717276a71caa0
Rishikesh <rishikesh@eximuz.com>
2023-02-18 21:04:52 +0530

    Add new landing page component

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,62 +1,63 @@
 <template>
-    <div>
-        <v-app>
-          <v-app-bar app color="black" dark dense>
-              <v-toolbar-title class="ml-10 pr-5">Uber</v-toolbar-title>
-              <v-menu left bottom>
-                <template v-slot:activator="{ on, attrs }">
-                  <v-btn icon v-bind="attrs" v-on="on" class="pl-10 pr-8" id="company-nav">
-                    Company
-                  </v-btn>
-                </template>
-                <v-list>
-                  <v-list-item v-for="n in 4" :key="n" @click="() => {}">
-                    Option {{ n }}
-                  </v-list-item>
-                </v-list>
-              </v-menu>
-              <v-btn id="login" class="pl-10"> safety </v-btn>
-              <v-btn id="login" > Help </v-btn>
-              <v-spacer></v-spacer>
-              <div class="pr-16">
-                <v-btn icon v-bind="attrs" v-on="on" id="products">
-                      <v-icon
-                      small
-                      color="white"
-                    >
-                      mdi-earth
-                    </v-icon>
-                      EN
-                    </v-btn>
-                <v-menu left bottom>
-                  <template v-slot:activator="{ on, attrs }">
-                    <v-btn icon v-bind="attrs" v-on="on" id="products">
-                      <v-icon
-                      small
-                      color="white"
-                    >
-                      mdi-dialpad
-                    </v-icon>
-                      Products
-                    </v-btn>
-                  </template>
-                  <v-list>
-                    <v-list-item v-for="n in 4" :key="n" @click="() => {}">
-                      Option {{ n }}
-                    </v-list-item>
-                  </v-list>
-                </v-menu>
-                <v-btn id="login"> Log in </v-btn>
-                <v-btn id="signup"> Sign up </v-btn>
-            </div>
-          </v-app-bar>
-      </v-app>
-    </div>
-  </template>
+  <div>
+    <v-app>
+      <v-app-bar app color="black" dark dense>
+        <v-toolbar-title class="ml-10 pr-5">Uber</v-toolbar-title>
+        <v-menu left bottom>
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn icon v-bind="attrs" v-on="on" class="pl-10 pr-8" id="company-nav">
+              Company
+            </v-btn>
+          </template>
+          <v-list>
+            <v-list-item v-for="n in 4" :key="n" @click="() => { }">
+              Option {{ n }}
+            </v-list-item>
+          </v-list>
+        </v-menu>
+        <v-btn id="login" class="pl-10"> safety </v-btn>
+        <v-btn id="login"> Help </v-btn>
+        <v-spacer></v-spacer>
+        <div class="pr-16">
+          <v-btn icon v-bind="attrs" v-on="on" id="products">
+            <v-icon small color="white">
+              mdi-earth
+            </v-icon>
+            EN
+          </v-btn>
+          <v-menu left bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn icon v-bind="attrs" v-on="on" id="products">
+                <v-icon small color="white">
+                  mdi-dialpad
+                </v-icon>
+                Products
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item v-for="n in 4" :key="n" @click="() => { }">
+                Option {{ n }}
+              </v-list-item>
+            </v-list>
+          </v-menu>
+          <v-btn id="login"> Log in </v-btn>
+          <v-btn id="signup"> Sign up </v-btn>
+        </div>
+      </v-app-bar>
+      <LandingPageComponent />
+    </v-app>
+    
+  </div>
   
-  <script>
-  export default {
-    name: 'HeaderComponent',
+</template>
+  
+<script>
+import LandingPageComponent from './LandingPageComponent.vue'
+export default {
+  name: 'HeaderComponent',
+  components: {
+    LandingPageComponent
+  },
 };
 
 </script>
---
+++ src/components/LandingPageComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="masterDiv">
+        <div class="imageDivOne">
+            <div class="box"></div>
+
+        </div>
+        <!-- <div class="imageDivTwo">
+
+        </div> -->
+    </div>
+
+</template>
+<script>
+
+</script>
